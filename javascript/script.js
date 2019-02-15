44c524b330871764eb8db5f54eafd6e8fc21e7dd
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-02-15 17:41:30 +0300

    Add a paragraph that changes when the user submits to show the triangle type or advise on what to do

--- javascript/script.js
+++ javascript/script.js
@@ -3,16 +3,18 @@ function triangle(){
   var b = parseInt(document.form1.second.value);
   var c = parseInt(document.form1.third.value);
   var form = document.getElementById('form1');
+  var text;
   if( a+b>c && a+c>b && b+c>a ){
     if(a==b && b==c && a==c){
-      alert("the values you entered will make an equilateral triangle.");
+      text = "The values you entered will make an equilateral triangle.";
     }else if (a!=b && b!=c && a!=c) {
-      alert("the values you entered will make a scalene triangle.");
+      text = "The values you entered will make a scalene triangle.";
     }else{
-      alert("the values you entered will make an isosceles triangle.");
+      text = "The values you entered will make an isosceles triangle.";
     }
   }else{
-    alert("Don't");
+    text = "The values you entered will not make a triangle. Try other values please. Make sure the sum of any two sides are not equal to or less than the third remaining side.";
   }
   form.reset();
+  document.getElementById("newp").innerHTML = text;
 }

8242c6118c85f925bce10d43654b67384944f55a
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-02-17 01:17:44 +0300

    Add an array to display the message

--- javascript/script.js
+++ javascript/script.js
@@ -3,17 +3,21 @@ function triangle(){
   var b = parseInt(document.form1.second.value);
   var c = parseInt(document.form1.third.value);
   var form = document.getElementById('form1');
-  var text;
+  var equilateral = "The values you entered will make an equilateral triangle.";
+  var scalene = "The values you entered will make a scalene triangle.";
+  var isosceles = "The values you entered will make an isosceles triangle.";
+  var other = "The values you entered will not make a triangle. Try other values please. Make sure the sum of any two sides are not equal to or less than the third remaining side."
+  var response = [equilateral, scalene, isosceles, other];
   if( a+b>c && a+c>b && b+c>a ){
     if(a==b && b==c && a==c){
-      text = "The values you entered will make an equilateral triangle.";
+      text = response[0];
     }else if (a!=b && b!=c && a!=c) {
-      text = "The values you entered will make a scalene triangle.";
+      text = response[1];
     }else{
-      text = "The values you entered will make an isosceles triangle.";
+      text = response[2];
     }
   }else{
-    text = "The values you entered will not make a triangle. Try other values please. Make sure the sum of any two sides are not equal to or less than the third remaining side.";
+    text = response[3];
   }
   form.reset();
   document.getElementById("newp").innerHTML = text;
